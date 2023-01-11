08456bf6586bb3ffa5e83cc850b69b4c3b619af1
William <hsiang.chuang@enhanceit.us>
2023-01-10 22:29:15 -0500

    Initial Commit

---
+++ Obj-C Json Parsing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Obj-C Json Parsing
+//
+//  Created by Consultant on 1/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
